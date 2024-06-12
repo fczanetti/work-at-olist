7f219c394eefcb5bbc7aace1676d06150a4cdee3
Fabio Zanetti <fczanetti@hotmail.com>
2024-06-12 15:51:09 -0300

    Implement book removal

--- work_at_olist/base/urls.py
+++ work_at_olist/base/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('books/<int:id>', views.book_read, name='book_read'),
     path('books/create', views.book_creation, name='book_creation'),
     path('books/update/<int:id>', views.book_update, name='book_update'),
+    path('books/delete/<int:id>', views.book_delete, name='book_delete'),
 ]
