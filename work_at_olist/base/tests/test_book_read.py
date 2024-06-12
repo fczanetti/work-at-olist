f3b42f6e965e9077c0b324394d3d9669cc21f89b
Fabio Zanetti <fczanetti@hotmail.com>
2024-06-12 13:31:58 -0300

    Implement book read
    - Correct book list view;
    - implement book read

---
+++ work_at_olist/base/tests/test_book_read.py
@@ -0,0 +1,39 @@
+import json
+
+import pytest
+from django.urls import reverse
+from http import HTTPStatus
+
+
+@pytest.fixture
+def resp_book_read_page(client, book):
+    """
+    Creates a request to read a book and
+    returns a response.
+    """
+    resp = client.get(reverse('base:book_read', args=(book.pk,)))
+    return resp
+
+
+def test_status_code_book_read_page(resp_book_read_page):
+    """
+    Certifies that book read page is
+    loaded successfully.
+    """
+    assert resp_book_read_page.status_code == HTTPStatus.OK
+
+
+def test_book_is_present_on_page(resp_book_read_page, book):
+    """
+    Certifies that the book is shown on read page.
+    """
+    assert json.loads(resp_book_read_page.content) == book.to_dict()
+
+
+def test_book_not_found_incorrect_id(client, db):
+    """
+    Certifies that a 404 response is returned
+    if tried to read a book with invalid id.
+    """
+    resp = client.get(reverse('base:book_read', args=(1234,)))
+    assert resp.status_code == HTTPStatus.NOT_FOUND
--- work_at_olist/base/urls.py
+++ work_at_olist/base/urls.py
@@ -5,7 +5,8 @@ from work_at_olist.base import views
 app_name = 'base'
 urlpatterns = [
     path('authors', views.authors, name='authors'),
-    path('books', views.books_read, name='books'),
+    path('books', views.books_list, name='books_list'),
+    path('books/<int:id>', views.book_read, name='book_read'),
     path('books/create', views.book_creation, name='book_creation'),
     path('books/update/<int:id>', views.book_update, name='book_update'),
 ]
